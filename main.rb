b0cbc927d89137c98da1e0fbef1ce919c9ed6e24
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-03-31 20:48:46 +0000

    Solved calculator with methods

--- main.rb
+++ main.rb
@@ -1,17 +1,39 @@
+def multiply(first_num, second_num)
+  first_num.to_f * second_num.to_f
+end
+
+def sum(first_num, second_num)
+  first_num.to_f + second_num.to_f
+end
+
+def divide(first_num, second_num)
+  first_num.to_f / second_num.to_f
+end
+
+def subtract(first_num, second_num)
+  first_num.to_f - second_num.to_f
+end
+
+def remainder(first_num, second_num)
+  first_num.to_f % second_num.to_f
+end
+
 puts "Simple Calculator"
 25.times { print "-"} 
 puts
 puts "Enter the first number"
-num_1 = gets.chomp
+first_num = gets.chomp
 puts "Enter the second number"
-num_2 = gets.chomp
+second_num = gets.chomp
 
-puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
+puts "The first number multiplied by the second number is #{multiply(first_num, second_num)}"
 
-puts "The first number added to the second number is #{num_1.to_i + num_2.to_i}"
+puts "The first number added to the second number is #{sum(first_num, second_num)}"
 
-puts "The first number divided by the second number is #{num_1.to_i / num_2.to_i}"
+puts "The first number divided by the second number is #{divide(first_num, second_num)}"
 
-puts "The first number subtracted by the second number is #{num_1.to_i - num_2.to_i}"
+puts "The first number subtracted by the second number is #{subtract(first_num, second_num)}"
 
-puts "The remainder of the first number of the second number is #{num_1.to_i % num_2.to_i}"
+puts "The remainder of the first number of the second number is #{remainder(first_num,second_num)}"
+
+
