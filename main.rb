162bb5670866b8f43323223db9295285bab763ef
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-03-31 17:58:09 +0000

    Completed the challenge

--- main.rb
+++ main.rb
@@ -5,4 +5,13 @@ puts "Enter the first number"
 num_1 = gets.chomp
 puts "Enter the second number"
 num_2 = gets.chomp
+
 puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
+
+puts "The first number added to the second number is #{num_1.to_i + num_2.to_i}"
+
+puts "The first number divided by the second number is #{num_1.to_i / num_2.to_i}"
+
+puts "The first number subtracted by the second number is #{num_1.to_i - num_2.to_i}"
+
+puts "The remainder of the first number of the second number is #{num_1.to_i % num_2.to_i}"
