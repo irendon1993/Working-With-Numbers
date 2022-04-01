bf84bbbbc67a8fcb4d64bf33c7894fdfba3ca63f
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-04-01 17:07:01 +0000

    Completed Conditionals challenge

--- main.rb
+++ main.rb
@@ -26,14 +26,30 @@ first_num = gets.chomp
 puts "Enter the second number"
 second_num = gets.chomp
 
-puts "The first number multiplied by the second number is #{multiply(first_num, second_num)}"
+puts "What do you want to do?"
+puts "Enter 1 for multiply, 2 for addition, 3 for subtraction, 4 for divison or 5 for remainder"
+user_entry = gets.chomp
 
-puts "The first number added to the second number is #{sum(first_num, second_num)}"
+if user_entry == "1"
+  puts "You have chosen to multiply"
+  puts "The first number multiplied by the second number is #{multiply(first_num, second_num)}"
 
-puts "The first number divided by the second number is #{divide(first_num, second_num)}"
+elsif user_entry == "2"
+  puts "You have chosen to add"
+  puts "The sum of the first number and the second number is #{sum(first_num, second_num)}"
 
-puts "The first number subtracted by the second number is #{subtract(first_num, second_num)}"
+elsif user_entry == "3"
+  puts "You have chosen to subtract" 
+  puts "The first number subtracted by the second number is #{subtract(first_num, second_num)}"
 
+elsif user_entry == "4"
+  puts "You have chosen to divide"
+  puts "The first number divided by the second number is #{divide(first_num, second_num)}"
+
+elsif user_entry == "5"
 puts "The remainder of the first number of the second number is #{remainder(first_num,second_num)}"
 
+else
+  puts "Invalid Entry"
+end 
 
